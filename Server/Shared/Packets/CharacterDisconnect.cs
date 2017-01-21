28f7f4b56f8e5aca4bd285b9d89000df15d5ff80
Mathias Danielsen <mathais234@gmail.com>
2017-01-21 13:06:27 +0100

    last one

--- Server/Shared/Packets/CharacterDisconnect.cs
+++ Server/Shared/Packets/CharacterDisconnect.cs
@@ -5,20 +5,27 @@ using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Shared.Packets {
-    public class CharacterDisconnect : INetworkPacket {
+    public class CharacterDisconnect : BaseNetworkPacket, INetworkPacket<CharacterDisconnect> {
+        public CharacterDisconnect() {
+            SocketId = -1;
+        }
+
         public CharacterDisconnect(int socketId) {
             SocketId = socketId;
         }
 
-        public int SocketId { get; }
+        public CharacterDisconnect FromByteArray(byte[] byteArray) {
+            var br = new BinaryReader(new MemoryStream(byteArray));
+
+            Header = (PacketHeader)br.ReadInt32();
+            SocketId = br.ReadInt32();
+
+            return this;
+        }
 
         public byte[] ToByteArray() {
             var bw = new BinaryWriter(new MemoryStream());
             bw.Write((int)PacketHeader.CharacterDisconnect);
-
-            var length = Marshal.SizeOf(SocketId); // not required
-
-            bw.Write(length);
 
             bw.Write(SocketId);
 
